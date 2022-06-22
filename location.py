83059c500432196e87e191c4d9679495dc99d7c5
HappyTsing <wangleqing00@qq.com>
2022-06-22 17:15:59 +0800

    录入位置信息

--- location.py
+++ location.py
@@ -1,14 +1,318 @@
 MEDIUM_COMMON = {
-    "1a": {  # 第一页第一张图
-        "hero": (),
-        "monkey_ace": (12, 31),
-        "sniper_monkey": ()
+    "1a": {
+        "hero": (666, 553),
+        "monkey_ace": (675, 359),
+        "sniper_monkey": (465, 549)
     },
-    "2d": {  # 第一页第一张图
-        "hero": (829, 355),
-        "monkey_ace": (647, 243),
-        "sniper_monkey": (939, 283)
-    }
+    "1b": {
+        "hero": (717, 220),
+        "monkey_ace": (885, 251),
+        "sniper_monkey": (749, 356)
+    },
+    "1c": {
+        "hero": (828, 513),
+        "monkey_ace": (591, 309),
+        "sniper_monkey": (790, 1032)
+    },
+    "1d": {
+        "hero": (454, 860),
+        "monkey_ace": (469, 189),
+        "sniper_monkey": (541, 483)
+    },
+    "1e": {
+        "hero": (546,417),
+        "monkey_ace": (575,217),
+        "sniper_monkey": (951,641)
+    },
+    "1f": {
+        "hero": (844,231),
+        "monkey_ace": (577,318),
+        "sniper_monkey": (1317,226)
+    },
+    "2a": {
+        "hero": (748, 725),
+        "monkey_ace": (467, 489),
+        "sniper_monkey": (455, 648)
+    },
+    "2b": {
+        "hero": (1413,567),
+        "monkey_ace": (1246,545),
+        "sniper_monkey": (836,360)
+    },
+    "2c": {
+        "hero": (1133,612),
+        "monkey_ace": (564,709),
+        "sniper_monkey": (1070,492)
+    },
+
+    "2d": {
+        "hero": (836,375),
+        "monkey_ace": (1063,480),
+        "sniper_monkey": (836,262)
+    },
+    "2e": {
+        "hero": (841,215),
+        "monkey_ace": (841,338),
+        "sniper_monkey": (841,509)
+    },
+    "2f": {
+        "hero": (1165,360),
+        "monkey_ace": (1255,490),
+        "sniper_monkey": (1038,315)
+    },
+    "3a": {
+        "hero": (723, 289),
+        "monkey_ace": (1047, 354),
+        "sniper_monkey": (976, 509)
+    },
+    "3b": {
+        "hero": (692,542),
+        "monkey_ace": (1004,170),
+        "sniper_monkey": (1107,353)
+    },
+    "3c": {
+        "hero": (497,846),
+        "monkey_ace": (701,1004),
+        "sniper_monkey": (636,633)
+    },
+
+    "3d": {
+        "hero": (874,642),
+        "monkey_ace": (809,502),
+        "sniper_monkey": (1000,492)
+    },
+    "3e": {
+        "hero": (1054,265),
+        "monkey_ace": (1049,362),
+        "sniper_monkey": (1048,487)
+    },
+    "3f": {
+        "hero": (843,202),
+        "monkey_ace": (1350,217),
+        "sniper_monkey": (831,999)
+    },
+    "4a": {
+        "hero": (412,501),
+        "monkey_ace": (492,397),
+        "sniper_monkey": (538,499)
+    },
+    "4b": {
+        "hero": (461,576),
+        "monkey_ace": (832,918),
+        "sniper_monkey": (472,558)
+    },
+
+    "5a": {
+        "hero": (358,396),
+        "monkey_ace": (429,733),
+        "sniper_monkey": (841,575)
+    },
+    "5b": {
+        "hero": (876,845),
+        "monkey_ace": (894,931),
+        "sniper_monkey": (1032,954)
+    },
+    "5c": {
+        "hero": (841,366),
+        "monkey_ace": (1540,701),
+        "sniper_monkey": (696,536)
+    },
+
+    "5d": {
+        "hero": (834,424),
+        "monkey_ace": (1250,601),
+        "sniper_monkey": (1445,997)
+    },
+    "5e": {
+        "hero": (1215,214),
+        "monkey_ace": (1458,552),
+        "sniper_monkey": (186,389)
+    },
+    "5f": {
+        "hero": (240,477),
+        "monkey_ace": (1499,670),
+        "sniper_monkey": (901,457)
+    },
+    "6a": {
+        "hero": (517,554),
+        "monkey_ace": (808,557),
+        "sniper_monkey": (1133,573)
+    },
+    "6b": {
+        "hero": (763,348),
+        "monkey_ace": (958,772),
+        "sniper_monkey": (631,307)
+    },
+    "6c": {
+        "hero": (657,524),
+        "monkey_ace": (435,610),
+        "sniper_monkey": (124,998)
+    },
+
+    "6d": {
+        "hero": (834,963),
+        "monkey_ace": (962,970),
+        "sniper_monkey": (747,965)
+    },
+    "6e": {
+        "hero": (856,373),
+        "monkey_ace": (906,206),
+        "sniper_monkey": (942,394)
+    },
+    "6f": {
+        "hero": (897,576),
+        "monkey_ace": (1012,680),
+        "sniper_monkey": (830,974)
+    },
+    "7a": {
+        "hero": (634,449),
+        "monkey_ace": (982,542),
+        "sniper_monkey": (1166,323)
+    },
+    "7b": {
+        "hero": (998,403),
+        "monkey_ace": (582,668),
+        "sniper_monkey": (1091,349)
+    },
+    "7c": {
+        "hero": (649,529),
+        "monkey_ace": (835,455),
+        "sniper_monkey": (827,173)
+    },
+
+    "7d": {
+        "hero": (1072,574),
+        "monkey_ace": (930,597),
+        "sniper_monkey": (926,933)
+    },
+    "7e": {
+        "hero": (851,504),
+        "monkey_ace": (1318,342),
+        "sniper_monkey": (617,528)
+    },
+
+    "8a": {
+        "hero": (783,530),
+        "monkey_ace": (1054,719),
+        "sniper_monkey": (262,122)
+    },
+    "8b": {
+        "hero": (683,313),
+        "monkey_ace": (1203,536),
+        "sniper_monkey": (1231,662)
+    },
+    "8c": {
+        "hero": (846,552),
+        "monkey_ace": (411,333),
+        "sniper_monkey": (1161,576)
+    },
+
+    "8d": {
+        "hero": (837,218),
+        "monkey_ace": (911,926),
+        "sniper_monkey": (822,753)
+    },
+    "8e": {
+        "hero": (427,553),
+        "monkey_ace": (1324,538),
+        "sniper_monkey": (837,536)
+    },
+    "8f": {
+        "hero": (1171,373),
+        "monkey_ace": (1213,532),
+        "sniper_monkey": (1136,248)
+    },
+    "9a": {
+        "hero": (1511,450),
+        "monkey_ace": (178,338),
+        "sniper_monkey": (263,161)
+    },
+    "9b": {
+        "hero": (1089,802),
+        "monkey_ace": (842,113),
+        "sniper_monkey": (847,228)
+    },
+    # 该地图无法放置飞行员
+    # "9c": {
+    #     "hero": (856,405),
+    #     "monkey_ace": (),
+    #     "sniper_monkey": ()
+    # },
+
+    "9d": {
+        "hero": (597,673),
+        "monkey_ace": (904,731),
+        "sniper_monkey": (1598,503)
+    },
+    "9e": {
+        "hero": (812,222),
+        "monkey_ace": (812,328),
+        "sniper_monkey": (781,672)
+    },
+    "9f": {
+        "hero": (777,483),
+        "monkey_ace": (824,589),
+        "sniper_monkey": (863,483)
+    },
+    "10a": {
+        "hero": (822,516),
+        "monkey_ace": (835,772),
+        "sniper_monkey": (822,311)
+    },
+
+    "11a": {
+        "hero": (841,290),
+        "monkey_ace": (832,168),
+        "sniper_monkey": (1239,481)
+    },
+    "11b": {
+        "hero": (1327,311),
+        "monkey_ace": (341,842),
+        "sniper_monkey": (484,819)
+    },
+    "11c": {
+        "hero": (740,506),
+        "monkey_ace": (597,404),
+        "sniper_monkey": (215,119)
+    },
+
+    "11d": {
+        "hero": (833,380),
+        "monkey_ace": (466,232),
+        "sniper_monkey": (839,741)
+    },
+    "11e": {
+        "hero": (402,297),
+        "monkey_ace": (1380,435),
+        "sniper_monkey": (895,180)
+    },
+    "11f": {
+        "hero": (995,439),
+        "monkey_ace": (1260,190),
+        "sniper_monkey": (983,501)
+    },
+    "12a": {
+        "hero": (866,373),
+        "monkey_ace": (823,803),
+        "sniper_monkey": (1062,503)
+    },
+    "12b": {
+        "hero": (731,437),
+        "monkey_ace": (795,760),
+        "sniper_monkey": (740,671)
+    },
+    "12c": {
+        "hero": (481,476),
+        "monkey_ace": (1522,259),
+        "sniper_monkey": (893,444)
+    },
+
+    "12d": {
+        "hero": (998,426),
+        "monkey_ace": (830,810),
+        "sniper_monkey": (686,664)
+    },
+
 }
 
 
@@ -19,6 +323,17 @@ class Location:
         self.detail = detail
 
     def get_monkeys_location(self, map_id: str):
-        if self.difficulty.lower() == "medium":
+        if self.difficulty.lower() == "easy":
+            if self.detail.lower() in ["standard", "primary_monkeys_only"]:
+                return 0
+            elif self.detail.lower() in ["deflation"]:
+                return 0
+        elif self.difficulty.lower() == "medium":
             if self.detail.lower() in ["standard", "reverse", "military_monkeys_only"]:
                 return MEDIUM_COMMON[map_id.lower()]
+            elif self.detail.lower() in ["apopalypse"]:
+                return 0
+        elif self.difficulty.lower() == "hard":
+            if self.detail.lower() in ["standard", "magic_monkeys_only", "double_hp_moabs", "half_cash",
+                                       "alternate_bloons_rounds", "impoppable", "chimps"]:
+                return 0
